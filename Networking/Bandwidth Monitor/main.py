2b8dd625edbcf580db249b69cad8eacfa03a88fc
Skullray <sarthak.singh99@gmail.com>
2017-05-15 10:58:23 +0530

    Bandwidth Monitor Done

--- Networking/Bandwidth Monitor/main.py
+++ Networking/Bandwidth Monitor/main.py
@@ -3,35 +3,54 @@ import psutil
 import matplotlib.pyplot as plt
 import numpy as np
 
-npa = np.array([])
+time = 30 #change this to record for longer sessios
+npd = np.zeros(time)
+npu = np.zeros(time)
 plt.ion()
 
 def main():
-    old_value = 0
+    old_valued = 0
+    old_valueu = 0
 
     while True:
-        new_value = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
+        new_valueu = psutil.net_io_counters().bytes_sent
+        new_valued = psutil.net_io_counters().bytes_recv
 
-        if old_value:
-            send_stat(new_value - old_value)
+        if old_valued:
+            send_statd(new_valued - old_valued)
+        if old_valueu:
+            send_statu(new_valueu - old_valueu)
 
-        old_value = new_value
-        global npa
+        old_valued = new_valued
+        old_valueu = new_valueu
+        global npd
+        global npu
 
-        plt.plot(npa)
+        plt.clf()
+        down, = plt.plot(npd, label='down')
+        up, = plt.plot(npu, label='up')
+        plt.legend([down, up], ['Download', 'Upload'])
         plt.ylabel('Data in MB')
-        plt.xlabel('Time')
+        plt.xlabel('Time (sec)')
         plt.pause(0.05)
 
-        time.sleep(1)
+        time.sleep(0.95)
 
 def convert_to_gbit(value):
     return value/1024./1024.*8
 
-def send_stat(value):
-    global npa
-    npa = np.append(npa,[convert_to_gbit(value)])
-    print(npa)
-    print ("%0.3f" % convert_to_gbit(value))
+def send_statd(value):
+    global npd
+    npd = np.delete(npd, 0)
+    npd = np.append(npd,[convert_to_gbit(value)])
+    #print(npa.shape)
+    #print ("%0.3f" % convert_to_gbit(value))
+
+def send_statu(value):
+    global npu
+    npu = np.delete(npu, 0)
+    npu = np.append(npu,[convert_to_gbit(value)])
+    #print(npa.shape)
+    #print ("%0.3f" % convert_to_gbit(value))
 
 main()
